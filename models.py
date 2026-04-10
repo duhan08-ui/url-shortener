3439b16414e1c0b97159995fc9757f4c1912637e
duhan08-ui <duhan08@gmail.com>
2026-04-10 17:06:38 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String
 from database import Base
 
 class URL(Base):
     __tablename__ = "urls"
+
     id = Column(Integer, primary_key=True, index=True)
-    original_url = Column(String, index=True, nullable=False)
+    original_url = Column(String, index=True)
     short_key = Column(String, unique=True, index=True)
     clicks = Column(Integer, default=0)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
